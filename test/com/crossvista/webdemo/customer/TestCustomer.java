fcb31ccf2f0fe5c3a560dfa8dfca254087c2076b
axonix <>
2023-06-21 17:43:57 +0300

    fix collisions during concurrent development

--- test/com/crossvista/webdemo/customer/TestCustomer.java
+++ test/com/crossvista/webdemo/customer/TestCustomer.java
@@ -9,19 +9,23 @@ public class TestCustomer extends BaseTest {
     private Customer customer;
 
     @Before
-    public void setCustomer(){
+    public void setCustomer() {
         customer = getCustomer();
     }
 
     @Test
     public void createCustomer() {
-
         Assert.assertEquals(CustomerField.FNAME.getValue(), customer.getFirstName());
         Assert.assertEquals(CustomerField.LNAME.getValue(), customer.getLastName());
         Assert.assertEquals(CustomerField.EMAIL.getValue(), customer.getEmail());
         Assert.assertEquals(CustomerField.PHONE.getValue(), customer.getPhone());
     }
 
+    @Test
+    @ConcurrentlyCollidedNode()
+    public void deleteCustomer_COLLIDED() {
+        System.out.println("delete content from BETA - BETA");
+    }
 
     @Test
     public void deleteCustomer() {
@@ -33,6 +37,9 @@ public class TestCustomer extends BaseTest {
         System.out.println("test on ALPHA something");
     }
 
-
-
+    @Test
+    @ConcurrentlyAddedNode()
+    public void moreTestFromBETA() {
+        System.out.println("test on BETA now !!!");
+    }
 }
